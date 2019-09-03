d0d49dc5f978f8a5b89e5b96e1d3df1ed131307e
Damien Keith Delgadillo <39660748+Damien-Delgadillo@users.noreply.github.com>
2019-09-03 00:21:09 -0700

    update

--- src/App.js
+++ src/App.js
@@ -3,15 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-		<h1 id="txt">Justin Olthof's Dildo Site</h1>
-		<h3 id="txt">Dark Web</h3>
-		<p id="txt">Get em now Cheap!</p>
-		<img id='img'src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2F7RXmNRr8x7I%2Fmaxresdefault.jpg&f=1" alt=""/>
-		<br/>
-		<br/>
-		<img id='img'src="https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fmadamesarka.com%2Fblog%2Fwp-content%2Fuploads%2Fst5.jpg&f=1" alt=""/>
-		<p id="txt">Starting price: 1999.99$</p>
+    <div className="App" id="txt">
+		<h1 >React Template</h1>
+		<p><strong>body</strong></p>
     </div>
   );
 }
